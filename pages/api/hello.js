3325b819c47f7a8eeb9b1d421117a775b8ff6236
Nice Guy <tabvim.dev@gmail.com>
2022-12-05 15:36:11 +0800

    add two two editor

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+// 场景就是转发吗? 用这里的api接口转发到其他服务里
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to Editor Demo Field!
         </h1>
 
         <p className={styles.description}>
@@ -22,35 +22,16 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="https://grapesjs.com/demo.html" className={styles.card}>
+            <h2>grapesjs &rarr;</h2>
+            <p>功能强大,支持导入导出</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a href="https://www.vvveb.com/vvvebjs/editor.html#" className={styles.card}>
+            <h2>VvvebJs &rarr;</h2>
+            <p>BootStrap加成,交互优雅</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
 
@@ -60,9 +41,9 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          Powered by
+          <span>
+            <a href='https://blog.ez-listen.app' target='_blank'>EZ listen</a>
           </span>
         </a>
       </footer>
